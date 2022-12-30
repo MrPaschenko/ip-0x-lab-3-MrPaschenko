4421f95390bdff74689fe2ea7a42d29cbde63ca0
mrpaschenko <paschenkod2@gmail.com>
2022-12-30 21:26:16 +0100

    Add input()

---
+++ inputOutput.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const fs = require('fs');
+module.exports = {
+  input,
+};
+
+function input(filePath) {
+  const string = fs.readFileSync(filePath, 'utf8');
+  const generalArray = string.split('\n');
+
+  const width = parseInt(generalArray[0].split(' ')[1]);
+  const height = parseInt(generalArray[0].split(' ')[0]);
+  const array = generalArray.slice(1, 1 + height);
+
+  return {
+    width,
+    height,
+    array,
+  };
+}
