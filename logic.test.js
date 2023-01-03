e20f0657d5447a8f55f5ee71dc5f1ada52d1a6e1
mrpaschenko <paschenkod2@gmail.com>
2023-01-03 14:48:23 +0100

    Add getOutput test

--- logic.test.js
+++ logic.test.js
@@ -129,3 +129,31 @@ test('Test getFinalField', () => {
 
   expect(getFinalField(field)).toEqual(expectedField);
 });
+
+test('Test getOutput', () => {
+  const field = new Field(8, 7, [
+    new Dot(2, 2),
+    new Dot(1, 3),
+    new Dot(2, 3),
+    new Dot(3, 3),
+    new Dot(2, 4)
+  ], [
+    new Dot(3, 4),
+    new Dot(3, 5),
+    new Dot(7, 5),
+    new Dot(0, 6),
+    new Dot(3, 6),
+    new Dot(4, 6),
+    new Dot(5, 6),
+    new Dot(6, 6),
+    new Dot(7, 6)
+  ]);
+
+  expect(getOutput(field)).toEqual('........\n' +
+    '........\n' +
+    '..p.....\n' +
+    '.ppp....\n' +
+    '..p#....\n' +
+    '...#...#\n' +
+    '#..#####');
+});
