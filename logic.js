6a41294db99e349c83affe4ee0b7c95cb265624a
mrpaschenko <paschenkod2@gmail.com>
2023-01-03 14:30:16 +0100

    Export Dot class

--- logic.js
+++ logic.js
@@ -103,4 +103,4 @@ function getOutput(field) {
   return newArray.join('\n');
 }
 
-module.exports = { Field, getOutput, getFigure, getLandscape };
+module.exports = { Dot, Field, getOutput, getFigure, getLandscape };
